1f435123f92ee5bde3644dcf2c29c9228341baae
Zach <eddyz1340@gmail.com>
2022-02-01 16:45:51 -0800

    adding ui

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,10 @@
 //generate computer selection
+let userSelection;
+let numberRounds = 5;
+let cpuWins = 0;
+let humanWins = 0;
+let roundsPlayed = 0;
+
 computerPlay = () => {
     let possibilites = ["rock", "paper", "scissors"];
     return possibilites[Math.floor(Math.random() * 3)];
@@ -38,27 +44,11 @@ playRound = (playerSelection, computerSelection) => {
     }  
 }
 
-//get user selection
-getUserPick = () => {
-    let userSelection;
-    while (!userSelection) {
-        userInput = prompt("Enter user selction:", "hint: rock, paper, or scissors");
-        userInput = userInput.toLowerCase();
-        if (userInput == "rock" || userInput == "paper" || userInput == "scissors") {
-            userSelection = userInput;
-        }
-    }
-    return userSelection;
-}
-
-game = (numberRounds) => {
-    let cpuWins = 0;
-    let humanWins = 0;
-    let roundsPlayed = 0;
-    while (roundsPlayed < numberRounds){
-        userSelection = getUserPick();
+loadGame = (userSelection) => {
+    let winner;
+    if (roundsPlayed < numberRounds) {
         cpuSelection = computerPlay();
-        let winner = playRound(userSelection, cpuSelection);
+        winner = playRound(userSelection, cpuSelection);
         console.log(winner);
         if (winner.includes("user")) {
             humanWins++;
@@ -67,8 +57,70 @@ game = (numberRounds) => {
             cpuWins++;
             roundsPlayed++;
         }
+        currentScore();
     }
-    console.log(`After ${numberRounds} rounds the score was User: ${humanWins} & CPU: ${cpuWins}`);
+    if (roundsPlayed == numberRounds) {
+        console.log();
+    }     
 }
 
-game(5);
+loadScore = () => {
+    const userScore = document.createElement("div");
+    userScore.innerHTML = `The current score is HUMAN: ${humanWins}, CPU: ${cpuWins}, with ${numberRounds - roundsPlayed} rounds remaining.`;
+    userScore.style.fontSize = "45px";
+    userScore.id = "score";
+    document.body.appendChild(userScore);
+}
+
+currentScore = () => {
+    let updateScore;
+    if (roundsPlayed < numberRounds) {
+        updateScore = `The current score is HUMAN: ${humanWins}, CPU: ${cpuWins}, with ${numberRounds - roundsPlayed} rounds remaining.`;
+    }
+    else if (roundsPlayed == numberRounds) {
+        updateScore = `After ${numberRounds} rounds the score was User: ${humanWins} & CPU: ${cpuWins}`;
+    }
+    let newScore = document.getElementById("score");
+    newScore.innerHTML = updateScore;  
+}
+
+makeButtons = () => {
+    let userSelection = "none";
+    const div = document.createElement("div");
+    const rock = document.createElement("button");
+    rock.innerHTML = "<span style='font-size:90px'>ROCK</span>";
+    rock.style.height = "450px";
+    rock.style.width = "450px";
+    rock.addEventListener("click", function(e) {
+        userSelection = "rock";
+        loadGame(userSelection);
+    });
+    div.appendChild(rock);
+
+    const paper = document.createElement("button");
+    paper.innerHTML = "<span style='font-size:90px'>PAPER</span>";
+    paper.style.height = "450px";
+    paper.style.width = "450px";
+    paper.addEventListener("click", function(e) {
+        userSelection = "paper";
+        loadGame(userSelection);
+    });
+    div.appendChild(paper);
+
+    const scissors = document.createElement("button");
+    scissors.innerHTML = "<span style='font-size:90px'>SCISSORS</span>";
+    scissors.style.height = "450px";
+    scissors.style.width = "450px";
+    scissors.addEventListener("click", function(e) {
+        userSelection = "scissors";
+        loadGame(userSelection);
+    });
+    div.appendChild(scissors);
+    document.body.appendChild(div);
+    loadScore();
+}
+
+makeButtons();
+
+
+
